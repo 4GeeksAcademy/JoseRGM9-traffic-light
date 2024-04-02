b22f11dcb46a6d234ac46b28153c7322b215d6cc
JoseRGM9 <157386833+JoseRGM9@users.noreply.github.com>
2024-04-02 17:45:45 +0000

    commit final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [seleccionarColor, setSeleccionarColor] = useState("red");
+	const [purpleActivo, setPurpleActivo] = useState(false);
+	
+	const botonColor = () => {
+		if (!purpleActivo) {
+			if (seleccionarColor === "red") {
+				setSeleccionarColor("yellow");
+			} else if (seleccionarColor === "yellow") {
+				setSeleccionarColor("green");
+			} else if (seleccionarColor === "green") {
+				setSeleccionarColor("red");
+			}
+		} else {
+			if (seleccionarColor === "red") {
+				setSeleccionarColor("yellow");
+			} else if (seleccionarColor === "yellow") {
+				setSeleccionarColor("green");
+			} else if (seleccionarColor === "green") {
+				setSeleccionarColor("purple");
+			} else if (seleccionarColor === "purple") {
+				setSeleccionarColor("red");
+			}
+		}
+	};
+
+	const botonPurple = () => {
+        setPurpleActivo(!purpleActivo);
+    };
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		<div className="contenedorBarra"></div>
+		<div className="container">
+			<div className="traffic-light">
+				<div onClick={() => setSeleccionarColor("red")} 
+				className={"light red" + ((seleccionarColor === "red") ? " glow" : "")}> 			
+				</div>
+				<div onClick={() => setSeleccionarColor("yellow")} 
+				className={"light yellow mt-3" + ((seleccionarColor === "yellow") ? " glow" : "")}></div>
+				<div onClick={() => setSeleccionarColor("green")} 
+				className={"light green mt-3" + ((seleccionarColor === "green") ? " glow" : "")}></div>
+				{purpleActivo && <div className={"light purple mt-3" + ((seleccionarColor === "purple") ? " glow" : "")}></div>}
+			</div>
+			<div>
+				<div className="contenedorBotones mt-4">
+					<div className="contenedorBotonCambiarColor">
+						<button onClick={botonColor} className="botonCambiarColor">Cambiar de color</button>
+					</div>
+					<div className="contenedorBotonPurpura mt-3"></div>
+					<button onClick={botonPurple} className="botonAgregarPurpura">{purpleActivo ? "Quitar Color Purpura" : "Añadir Color Purpura"}</button>
+				</div>
+			</div>
+		</div>
 		</div>
 	);
 };
